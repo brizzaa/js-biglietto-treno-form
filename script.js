f6c23a18e1b59abe6d0c58a972d322643194ea61
brizzaa <brizzantex@gmail.com>
2025-03-02 19:18:31 +0100

    feat:first-commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// Il programma dovrà mostrare una form da compilare usando i corretti campi HTML di input con cui chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero. Utilizzate Bootstrap come libreria UI.
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+// il prezzo del biglietto è definito in base ai km (0.21 € al km)
+//va applicato uno sconto del 20% per i minorenni
+//va applicato uno sconto del 40% per gli over 65.
+//Il recap dei dati e l'output del prezzo finale va stampato in pagina (formattato con massimo due decimali, per indicare centesimi sul prezzo).
+//Vi consigliamo di seguire questo ordine nella risoluzione dell'esercizio:
+
+//2. Al click sul bottone "calcola" prelevare tutti i dati dagli input e stamparli in console.
+//3. Eseguire il calcolo del prezzo e stamparlo in console.
+//4. Stampare il risultato in pagina. */
