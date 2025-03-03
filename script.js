b22a142dd4fb2366d1606b5fae51700f3b6144f7
brizzaa <brizzantex@gmail.com>
2025-03-04 00:07:45 +0100

    feat: commit

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
-// Il programma dovrà mostrare una form da compilare usando i corretti campi HTML di input con cui chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero. Utilizzate Bootstrap come libreria UI.
-// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
-// il prezzo del biglietto è definito in base ai km (0.21 € al km)
-//va applicato uno sconto del 20% per i minorenni
-//va applicato uno sconto del 40% per gli over 65.
-//Il recap dei dati e l'output del prezzo finale va stampato in pagina (formattato con massimo due decimali, per indicare centesimi sul prezzo).
-//Vi consigliamo di seguire questo ordine nella risoluzione dell'esercizio:
+// prendo bottone 
+const bottoneInput = document.getElementById("submit-btn")
 
-//2. Al click sul bottone "calcola" prelevare tutti i dati dagli input e stamparli in console.
-//3. Eseguire il calcolo del prezzo e stamparlo in console.
-//4. Stampare il risultato in pagina. */
+// se viene cliccato allora parte la funzione
+bottoneInput.addEventListener("click",function bottone(){
+    // rendo le variabili piu capibili
+    const kmInput = document.getElementById("km").value;
+    const anniInput = document.getElementById("anni").value;
+    // trasformo in numeri
+    let km = parseInt(kmInput);
+    let anni = parseInt(anniInput);
+    // calcolo prezzo e sconti e poi ritorno in schermo la value 
+    let prezzoBiglietto = 0.21 * km;
+    if (anni < 18){
+        let sconto = (prezzoBiglietto / 100) * 20
+        prezzoBiglietto = prezzoBiglietto - sconto;
+        console.log(prezzoBiglietto.toFixed(2));
+        
+    }
+    else if (anni >= 65){
+        let sconto = (prezzoBiglietto / 100) * 40;
+        prezzoBiglietto = prezzoBiglietto - sconto;
+        console.log(prezzoBiglietto.toFixed(2));
+        
+    }
+    else{
+        console.log(prezzoBiglietto.toFixed(2))
+    }
+})
