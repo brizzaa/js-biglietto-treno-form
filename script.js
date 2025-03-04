1becba16fdbfbad8ecd845e6f2e9d92cefca66f2
brizzaa <brizzantex@gmail.com>
2025-03-04 16:01:51 +0100

    feat: exercise-completed

--- script.js
+++ script.js
@@ -3,32 +3,51 @@ const bottoneInput = document.getElementById("submit-btn")
 const outputDiv = document.getElementById("output")
 
 // se viene cliccato allora parte la funzione
-bottoneInput.addEventListener("click",function bottone(){
+bottoneInput.addEventListener("click", function bottone() {
     // rendo le variabili piu capibili
     const kmInput = document.getElementById("km").value;
     const anniInput = document.getElementById("anni").value;
     // trasformo in numeri
-    let km = parseInt(kmInput);
-    let anni = parseInt(anniInput);
-    // calcolo prezzo e sconti e poi ritorno in schermo la value 
+    const km = parseInt(kmInput);
+    const anni = parseInt(anniInput);
     let prezzoBiglietto = 0.21 * km;
-    if (anni < 18){
+
+    if (isNaN(anni) === true || isNaN(km) === true) {
+        outputDiv.innerHTML = `
+        <div class="row justify-content-center mt-4">inserisci dei valori validi!!</div>
+        `
+    }
+    else
+
+    // calcolo prezzo e sconti e poi ritorno in schermo la value 
+    if (anni < 18) {
+
         let sconto = (prezzoBiglietto / 100) * 20
         prezzoBiglietto = prezzoBiglietto - sconto;
         console.log(prezzoBiglietto.toFixed(2));
+
         outputDiv.innerHTML = `
          <div class="row justify-content-center mt-4">Prezzo ${prezzoBiglietto.toFixed(2)}€</div>
         `
-        
+
     }
-    else if (anni >= 65){
+    else if (anni >= 65) {
+
         let sconto = (prezzoBiglietto / 100) * 40;
         prezzoBiglietto = prezzoBiglietto - sconto;
-        console.log(prezzoBiglietto.toFixed(2));
-        
+
+        outputDiv.innerHTML = `
+         <div class="row justify-content-center mt-4">Prezzo ${prezzoBiglietto.toFixed(2)}€</div>
+        `
+
     }
-    else{
-        
-        console.log(prezzoBiglietto.toFixed(2))
+    else {
+        outputDiv.innerHTML = `
+         <div class="row justify-content-center mt-4">Prezzo ${prezzoBiglietto.toFixed(2)}€</div>
+        `
     }
+
+
 })
+
+
